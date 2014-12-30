c93b192154d1adb717032e3d81f9dd68b0e17d96
unknown <c1292720@m2055881-w7.tlv.gov.il>
2014-12-30 10:12:37 +0200

    initial commit for reporting bug to Google Android

---
+++ TestReorderActivities/gen/com/davidtest/testreorderactivities/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.davidtest.testreorderactivities;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ TestReorderActivities/src/com/davidtest/testreorderactivities/MainActivity.java
@@ -0,0 +1,53 @@
+package com.davidtest.testreorderactivities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button reorderBtn = (Button)findViewById(R.id.reorder_button);
+        TextView title = (TextView)findViewById(R.id.title);
+        title.setText("First Activity");
+        reorderBtn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				Intent i= new Intent(MainActivity.this, SecondActivity.class);
+				i.addFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+				startActivity(i);
+			}
+		});
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
